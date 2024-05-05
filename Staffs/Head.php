45e0cb7a86c668dc834b6094f5381910d5f98463
Anjali <140676692+p-v-anjali@users.noreply.github.com>
2024-05-05 21:01:59 +0530

    Add files via upload

---
+++ Staffs/Head.php
@@ -0,0 +1,124 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>medical</title>
+    <link rel="icon" href="../Assets/Temp/Main/img/favicon.png">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="../Assets/Temp/Main/css/bootstrap.min.css">
+    <!-- animate CSS -->
+    <link rel="stylesheet" href="../Assets/Temp/Main/css/animate.css">
+    <!-- owl carousel CSS -->
+    <link rel="stylesheet" href="../Assets/Temp/Main/css/owl.carousel.min.css">
+    <!-- themify CSS -->
+    <link rel="stylesheet" href="../Assets/Temp/Main/css/themify-icons.css">
+    <!-- flaticon CSS -->
+    <link rel="stylesheet" href="../Assets/Temp/Main/css/flaticon.css">
+    <!-- magnific popup CSS -->
+    <link rel="stylesheet" href="../Assets/Temp/Main/css/magnific-popup.css">
+    <!-- nice select CSS -->
+    <link rel="stylesheet" href="../Assets/Temp/Main/css/nice-select.css">
+    <!-- swiper CSS -->
+    <link rel="stylesheet" href="../Assets/Temp/Main/css/slick.css">
+    <!-- style CSS -->
+    <link rel="stylesheet" href="../Assets/Temp/Main/css/style.css">
+</head>
+
+<body>
+    <!--::header part start::-->
+    <header class="main_menu home_menu">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-12">
+                    <nav class="navbar navbar-expand-lg navbar-light">
+                        <a class="navbar-brand" href="index.html"> <img src="../Assets/Temp/Main/img/logo.png" alt="logo"> </a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse"
+                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+
+                        <div class="collapse navbar-collapse main-menu-item justify-content-center"
+                            id="navbarSupportedContent">
+                            <ul class="navbar-nav align-items-center">
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="index.php">Home</a>
+                                </li>
+                               <!-- <li class="nav-item">
+                                    <a class="nav-link" href="about.html">about</a>
+                                </li>-->
+                              <!--  <li class="nav-item">
+                                    <a class="nav-link" href="Doctor.html">Doctors</a>
+                                </li>-->
+
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="blog.html" id="navbarDropdown"
+                                        role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                      Profile
+                                    </a>
+                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="MyProfile.php"> My Profile</a>
+                                        <a class="dropdown-item" href="EditProfile.php"> Edit Profile</a>
+										<a class="dropdown-item" href="ChangePassword.php"> Change Password</a>
+
+                                    </div>
+                                </li>
+                                 <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="blog.html" id="navbarDropdown"
+                                        role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Patient List
+                                    </a>
+                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="AssignedPatientList.php"> Assigned Patient List</a>
+                                    
+                                    </div>
+                                </li>
+                                 <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="blog.html" id="navbarDropdown"
+                                        role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Complaint and Feedback
+                                    </a>
+                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="Complaint.php"> Complaint</a>
+                                        <a class="dropdown-item" href="ComplaintReply.php">Complaint Reply</a>
+                                        <a class="dropdown-item" href="Feedback.php">Feedback</a>
+                                        
+                                    </div>
+                                </li>
+                                 <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="blog.html" id="navbarDropdown"
+                                        role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Rating
+                                    </a>
+                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="Rating.php">Rating</a>
+                                      
+                                    </div>
+                                </li>
+                                 
+                               <!--<!-- <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="blog.html" id="navbarDropdown_1"
+                                        role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        blog
+                                    </a>
+                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown_1">
+                                        <a class="dropdown-item" href="blog.html">blog</a>
+                                        <a class="dropdown-item" href="single-blog.html">Single blog</a>
+                                    </div>
+                                </li>
+
+                                <li class="nav-item">
+                                    <a class="nav-link" href="contact.html">Contact</a>
+                                </li>-->-->
+                            </ul>
+                        </div>
+                        <a class="btn_2 d-none d-lg-block" href="Login/Login.php">Login</a>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- Header part end-->
