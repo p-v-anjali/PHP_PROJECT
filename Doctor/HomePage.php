84e5cee4fe6f20ab50a33f1847d1c6e700e53bc3
Anjali <140676692+p-v-anjali@users.noreply.github.com>
2024-05-07 11:40:19 +0530

    Add files via upload

--- Doctor/HomePage.php
+++ Doctor/HomePage.php
@@ -1,7 +1,7 @@
 <?php
-ob_start();
+
 session_start();
-include("Head.php");
+
 ?>
 
 
@@ -11,26 +11,47 @@ include("Head.php");
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Home Page</title>
 </head>
-<body  style="background:url(docter.jpeg); background-repeat: no-repeat;" >
-<table align="center" cellpadding="10" style="border-collapse:collapse">
 
-<tr>
-<font size =6 color="black"
-<br/><br/><br/>
-<td><p><i><h1><b><u><center>Welcome : Dr <?php echo $_SESSION["doctorname"];?></center><i><h1><b><u></p></td>
-</font>
-</tr>
-</table>
-<br/><br/>
+<body>
 
-
-
-
-
-<?php
-include("Foot.php");
-ob_flush();
-?>
+<p>&nbsp;</p>
+<form id="form1" name="form1" method="post" action="">
+<table border="1" align="center" cellpadding="10" style="border-collapse:collapse">
+<p>&nbsp;</p>
+	<tr>
+	  <td><i><h1><b><u>Welcome : Dr.<?php echo $_SESSION["doctorname"]?><i><h1><b><u></td>
+  </tr>
+	<tr>
+    <td><a href="MyProfile.php"><b>Profile</b></a></td>
+  </tr>
+  <tr>
+    <td><a href="EditProfile.php"><b>Edit Profile</b></a></td>
+  </tr>
+  <tr>
+    <td><a href="ChangePassword.php"><b>Change password</b></a></td>
+  </tr>
+  <tr>
+    <td><a href="ViewPatientRequestList.php"><b>View Patient Request List</b></a></td>
+  </tr>
+   <tr>
+    <td><a href="PatientRequestAcceptedList.php"><b>Patient Request Accepted List</b></a></td>
+  </tr>
+  <tr>
+     <td><a href="PatientRequestRejectedList.php"><b>Patient Request Rejected List</b></a></td>
+  </tr>
+	 <tr>
+    <td><a href="ScheduledPatient.php"><b>Scheduled Patient</b></a></td>
+  </tr>
+  <tr>
+    <td><a href="Complaint.php"><b>Complaint</b></a></td>
+  </tr>
+   <tr>
+    <td><a href="ComplaintReply.php"><b>Complaint Reply</b></a></td>
+  </tr>
+  <tr>
+    <td><a href="Feedback.php"><b>Feedback</b></a></td>
+  </tr
+></table>
+<p>&nbsp;</p>
 </body>
 </html>
-
